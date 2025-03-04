effc76073c2750eae8a80925e41e46ad5d18d525
maheshwaran-tr <maheswaran6380@gmail.com>
2025-03-04 09:30:06 +0530

    htmlpdf

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -1,32 +1,50 @@
-import puppeteer from "puppeteer";
+// import puppeteer from "puppeteer";
 import ejs from "ejs";
 import studentService from "../services/studentService.js";
+import pdf from "html-pdf";
+import fs from "fs/promises";
 
 const genPdfAllStudents = async () => {
     const students = await studentService.getAllStudents();
     const studentHeaders = ["RollNo", "RegNo", "Name", "Department", "Email", "PhoneNo"];
-    const htmlContent = await ejs.renderFile("assets/template.ejs", { studentHeaders, students });
-    const browser = await puppeteer.launch({
-        headless: "new",
-        args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
     
+    // Load EJS Template
+    const htmlTemplate = await fs.readFile("assets/template.ejs", "utf-8");
+    const htmlContent = ejs.render(htmlTemplate, { studentHeaders, students });
     
-    const page = await browser.newPage();
-    await page.setContent(htmlContent);
-    const pdfBuffer = await page.pdf({
-        format: 'A4',
-        printBackground: true,
-        margin: {
-            top: '10mm',
-            right: '10mm',
-            bottom: '10mm',
-            left: '10mm'
-        }
+    return new Promise((resolve, reject) => {
+        pdf.create(htmlContent, { format: 'A4' }).toBuffer((err, buffer) => {
+            if (err) reject(err);
+            else resolve(buffer);
+        });
     });
-    await browser.close();
-    return pdfBuffer;
 };
+
+// const genPdfAllStudents = async () => {
+//     const students = await studentService.getAllStudents();
+//     const studentHeaders = ["RollNo", "RegNo", "Name", "Department", "Email", "PhoneNo"];
+//     const htmlContent = await ejs.renderFile("assets/template.ejs", { studentHeaders, students });
+//     const browser = await puppeteer.launch({
+//         headless: "new",
+//         args: ["--no-sandbox", "--disable-setuid-sandbox"]
+//     });
+    
+    
+//     const page = await browser.newPage();
+//     await page.setContent(htmlContent);
+//     const pdfBuffer = await page.pdf({
+//         format: 'A4',
+//         printBackground: true,
+//         margin: {
+//             top: '10mm',
+//             right: '10mm',
+//             bottom: '10mm',
+//             left: '10mm'
+//         }
+//     });
+//     await browser.close();
+//     return pdfBuffer;
+// };
 
 
 export default {genPdfAllStudents};
